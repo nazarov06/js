4bb8f0798605079159e7a7058047f747ab2c63c0
Ozodbek Nazarov <asattorov712@gmail.com>
2023-01-13 15:43:42 +0300

    JavaScript Homework

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let  name="Ozodbek";
+let surName = 'Nazarov';
+let laptopName = 'Acer';
+let age = 12;
+let birthYear = 2007;
+
+console.log(name);
+console.log(surName);
+console.log(laptopName);
+console.log(age);
+console.log(birthYear);
