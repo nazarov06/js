aebd1ed11e02911c087089250972972afb2779a0
Ozodbek Nazarov <asattorov712@gmail.com>
2023-01-20 10:03:11 +0500

    if else

--- js/script.js
+++ js/script.js
@@ -1,13 +1,64 @@
-let name = prompt('ismingiz nima?');
-let surName = prompt('familiyanggiz nima?') ;
-let laptopName = prompt ('noutbookiz?');
-let age = prompt ('yoshinggiz?');
-let birthYear = prompt ('nechinchi yilsiz?');
+let userName = prompt('Ismingiz')
+let age = +prompt('Yoshingiz')
 
-let obj= {}
-obj.name = name;
-obj.surName = surName;
-obj.laptopName = laptopName;
-obj.age = age;
-obj.birthYear = birthYear;
-console.log(obj)
+let userName1 = prompt('Ismingiz')
+let age1 = +prompt('Yoshingiz')
+
+let user1 = {
+    name: userName,
+    age: age,
+}
+
+let user2 = {
+    name: userName1,
+    age: age1
+}
+
+if (user1.age > user2.age) {
+    alert(`${user1.name} ${user2.name} dan katta`)
+} else if (user1.age < user2.age) {
+    alert(`${user1.name} ${user2.name} dan kichkina`)
+} else if (user1.age == user2.age) {
+    alert(`${user1.name} va ${user2.name} ning yoshlari teng`)
+} else {
+    alert('Error')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let firstNumber = +prompt('1-sonni kiriting')
+let operator = prompt('Matematik amalni kiriting (+ - * /)')
+let secondNumber = +prompt('2-sonni kiriting')
+
+if (!isNaN(Number(firstNumber)) && !isNaN(Number(secondNumber))) {
+    if (operator === '+') {
+        alert(firstNumber + secondNumber);
+    } else if (operator === '-') {
+        alert(firstNumber - secondNumber);
+    } else if (operator === '*') {
+        alert(firstNumber * secondNumber);
+    } else if (operator === '/') {
+        alert(firstNumber / secondNumber);
+    } else {
+        alert("Amal noto'ri")
+    }
+} else {
+    alert('Boshidan urining')
+}
